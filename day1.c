ae0d78e182f855b0ce511c6412ea88544def618d
mwansachibale <mikemwansa11@gmail.com>
2025-10-02 09:44:20 +0200

    Update day1.c

--- day1.c
+++ day1.c
@@ -1,15 +1,16 @@
 
-#include <stdio.h>
+#include <stdio.h> 
+
 int main(){
-char name[50];
-int age;
-printf("Enter name:");
-scanf("%s", name);
-printf("Enter age:");
-scanf("%d", &age); 
-printf("Hello, %s,  you are %d years old\n", name, age);
-printf("your favorite hobby is coding");
-return 0;
+   char name[50];
+   int age;
+   printf("Enter name:");
+   scanf("%s", name);
+   printf("Enter age:");
+   scanf("%d", &age); 
+   printf("Hello, %s,  you are %d years old\n", name, age);
+   printf("your favorite hobby is coding");
+   return 0;
 }
 
 
